7262bf27af43c62985e800ac05f4ad4cb1b311f6
LIKHITHREDDYADDELA <laddela@albany.edu>
2025-06-09 20:31:51 -0400

    made html,css additions and deletions

--- frontend/taskmanager-frontend/src/app/project/task-list/task-list.component.ts
+++ frontend/taskmanager-frontend/src/app/project/task-list/task-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from '../../task.model';
 
 @Component({
   selector: 'app-task-list',
@@ -8,5 +9,51 @@ import { Component } from '@angular/core';
   styleUrl: './task-list.component.css'
 })
 export class TaskListComponent {
+ tasks: Task[] = [{
+
+  id: 1,
+name: "Develop Front end",
+description : "this is the description",
+completed : true,
+dueDate: new Date('06-03-2025'),
+project : 1
+
+},
+{
+
+  id: 1,
+name: "Design Wireframe",
+description : "this is the description",
+completed : false,
+dueDate: new Date('06-25-2025'),
+project : 1
+
+},
+{
+
+  id: 1,
+name: "Design Wireframe",
+description : "this is the description",
+completed : false,
+dueDate: new Date('06-25-2025'),
+project : 1
+
+},{
+
+  id: 1,
+name: "Design Wireframe",
+description : "this is the description",
+completed : false,
+dueDate: new Date('06-25-2025'),
+project : 1
+
+}
+
+]
+
+
+
+
+
 
 }
