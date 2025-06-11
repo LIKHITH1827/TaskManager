9e53e20c3577b1664b22e476f566bf14d9acdc05
LIKHITHREDDYADDELA <laddela@albany.edu>
2025-06-10 20:03:40 -0400

    Added task service

---
+++ frontend/taskmanager-frontend/src/app/task.service.ts
@@ -0,0 +1,75 @@
+import { Injectable } from '@angular/core';
+import { Task } from './task.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  tasks: Task[] = [{
+
+    id: 1,
+  name: "Develop Front end",
+  description : "this is the description",
+  completed : true,
+  dueDate: new Date('06-03-2025'),
+  project : 1
+
+  },
+  {
+
+    id: 2,
+  name: "Design Wireframe",
+  description : "this is the description",
+  completed : false,
+  dueDate: new Date('06-25-2025'),
+  project : 3
+
+  },
+  {
+
+    id: 3,
+  name: "Design Wireframe",
+  description : "this is the description",
+  completed : false,
+  dueDate: new Date('06-25-2025'),
+  project : 2
+
+  },{
+
+    id: 4,
+  name: "Design Wireframe",
+  description : "this is the description",
+  completed : false,
+  dueDate: new Date('06-25-2025'),
+  project : 1
+
+  }
+
+  ]
+
+  getTasks(){
+    return this.tasks;
+  }
+
+  addTask(task: Task){
+    this.tasks.push(task);
+    return this.tasks;
+  }
+
+  updateTask(newTask:Task){
+    const updatedIndex= this.tasks.findIndex((task)=>task.id===newTask.id);
+    this.tasks[updatedIndex]=newTask;
+    return this.tasks;
+  }
+
+
+deleteTask(id:number){
+  const taskIndex= this.tasks.findIndex((task)=>task.id===id);
+  this.tasks.splice(taskIndex,1);
+}
+
+constructor(){
+
+}
+}
