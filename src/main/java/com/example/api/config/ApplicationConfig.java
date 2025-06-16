b10fef794407d0927278a3c96264997f09b99289
LIKHITHREDDYADDELA <laddela@albany.edu>
2025-06-16 17:21:15 -0400

    added Applicationconfig class

---
+++ src/main/java/com/example/api/config/ApplicationConfig.java
@@ -0,0 +1,34 @@
+package com.example.api.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.example.api.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    
+	private final UserRepository repository;
+	
+	public ApplicationConfig(UserRepository repository) {
+		// TODO Auto-generated constructor stub
+		this.repository=repository;
+	}
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		
+		return username -> repository.findByEmail(username)
+				.orElseThrow(()-> new UsernameNotFoundException("User not found"));
+		
+		
+	}
+	
+}
