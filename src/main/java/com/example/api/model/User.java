943eed0757d7af90f2b6a824be88ab17818336ee
LIKHITHREDDYADDELA <laddela@albany.edu>
2025-06-16 12:14:58 -0400

    added UserRepository

--- src/main/java/com/example/api/model/User.java
+++ src/main/java/com/example/api/model/User.java
@@ -1,11 +1,17 @@
 package com.example.api.model;
 
 import java.util.Collection;
+import java.util.List;
+
+import javax.management.relation.Role;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -30,10 +36,13 @@ public class User implements UserDetails {
 	private String email;
 	private String password;
 	
+	@Enumerated(EnumType.STRING)
+	private Role role;
+	
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		// TODO Auto-generated method stub
-		return null;
+		return List.of(new SimpleGrantedAuthority(role.getRoleName()));
 	}
 	
 	@Override
