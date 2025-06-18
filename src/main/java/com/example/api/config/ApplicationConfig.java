a9ef0cdcf50a24bc8ce2416102572d513f4ac108
LIKHITHREDDYADDELA <laddela@albany.edu>
2025-06-18 15:34:03 -0400

    implemented AuthenticationProvider

--- src/main/java/com/example/api/config/ApplicationConfig.java
+++ src/main/java/com/example/api/config/ApplicationConfig.java
@@ -1,10 +1,18 @@
 package com.example.api.config;
 
+import java.security.PrivateKey;
+import java.security.Provider;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.webauthn.api.PublicKeyCose;
 
 import com.example.api.repository.UserRepository;
 
@@ -17,7 +25,7 @@ public class ApplicationConfig {
     @Autowired
 	private UserRepository repository;
 	
-	
+   
 	
 	@Bean
 	public UserDetailsService userDetailsService() {
@@ -26,6 +34,18 @@ public class ApplicationConfig {
 				.orElseThrow(()-> new UsernameNotFoundException("User not found"));
 		
 		
+	}
+	
+	@Bean
+	public AuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService());
+		provider.setPasswordEncoder(passwordEncoder());
+		return provider;
+	}
+	
+	@Bean
+	public PasswordEncoder  passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 	
 }
