d23cedad98e577dfaab9d836d865eec688474e1f
LIKHITHREDDYADDELA <laddela@albany.edu>
2025-06-19 16:24:58 -0400

    added login endpoint in AuthController

--- src/main/java/com/example/api/controller/AuthController.java
+++ src/main/java/com/example/api/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.example.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,14 +17,23 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*")
 public class AuthController {
 
 	
 	  @Autowired 
 	  private AuthService service;
 	  
-	  @PostMapping("/register") public ResponseEntity<AuthenticationResponse>
-	  register(@RequestBody RegisterRequest request){ return
-	  ResponseEntity.ok(service.register(request)); }
+	  @PostMapping("/register") 
+	  public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request){ 
+		  return ResponseEntity.ok(service.register(request));
+		  
+	  }
+	  
+	  @PostMapping("/login")
+	  public ResponseEntity<AuthenticationResponse> login(@RequestBody AuthenticationRequest request){
+		  
+		  return ResponseEntity.ok(service.login(request));
+	  }
 	 
 }
