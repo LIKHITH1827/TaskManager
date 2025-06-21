c2d532213836eeb5dffaba16dd023fd2bdea1e42
LIKHITHREDDYADDELA <laddela@albany.edu>
2025-06-20 20:25:24 -0400

    added routes array

--- frontend/taskmanager-frontend/src/app/app.routes.ts
+++ frontend/taskmanager-frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ProjectComponent } from './project/project.component';
+import { RegisterComponent } from './register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {path:'', component: ProjectComponent},
+  {path:'register', component: RegisterComponent}
+
+];
