a6b4038fe4b752759e6c39852e10d4520be3fc55
LIKHITHREDDYADDELA <laddela@albany.edu>
2025-07-01 23:53:32 -0400

    added validations in model class

--- src/main/java/com/example/api/model/Task.java
+++ src/main/java/com/example/api/model/Task.java
@@ -15,6 +15,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -29,7 +30,7 @@ public class Task {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	
+	@Size(min = 2, max = 30, message = "Name must be between 2 and 30 characters")
 	private String name;
 	private String description;
 	@Column(nullable = false)
